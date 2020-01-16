258458d10d277228b801af4cee398f04176d4e24
wcnnkh <247044007@qq.com>
2020-01-16 18:52:34 +0800

    fix

--- src/main/java/indi/scw/book/service/impl/JsoupCacheManager.java
+++ src/main/java/indi/scw/book/service/impl/JsoupCacheManager.java
@@ -7,16 +7,16 @@ import org.jsoup.nodes.Document;
 
 import scw.core.utils.XTime;
 import scw.data.file.AutoRefreshFileCache;
-import scw.data.file.SerialzerableClientHttpInputMessageCacheConvert;
-import scw.net.http.client.SerialzerableClientHttpInputMessage;
+import scw.data.file.SerializableClientHttpInputMessageCacheConvert;
+import scw.net.http.client.SerializableClientHttpInputMessage;
 
 public final class JsoupCacheManager extends AutoRefreshFileCache {
 	protected JsoupCacheManager() {
-		super((int) (XTime.ONE_DAY * 30), new SerialzerableClientHttpInputMessageCacheConvert());
+		super((int) (XTime.ONE_DAY * 30), new SerializableClientHttpInputMessageCacheConvert());
 	}
 
 	public Document getDocument(String url) {
-		SerialzerableClientHttpInputMessage message = getAndTouch(url);
+		SerializableClientHttpInputMessage message = getAndTouch(url);
 		Document document = Jsoup.parse(message.toString());
 		return document;
 	}
