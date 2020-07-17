4ea192dec0a1587b0ac143589152d7136326c163
wcnnkh <247044007@qq.com>
2020-07-17 18:29:38 +0800

    支持MainArgs

--- src/main/java/indi/scw/book/BookApplication.java
+++ src/main/java/indi/scw/book/BookApplication.java
@@ -4,6 +4,6 @@ import scw.application.MainApplication;
 
 public class BookApplication {
 	public static void main(String[] args) {
-		MainApplication.run(BookApplication.class);
+		MainApplication.run(BookApplication.class, args);
 	}
 }
